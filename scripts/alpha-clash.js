2c6a8a643e6a5c9b1d00b42dd21351a13cfafebf
RahulSaha1998 <rahulsaha87928@gmail.com>
2025-01-01 20:58:14 +0600

    added function for random alphabets

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,20 @@
-function play(){
-    console.log('play start now');
+// function play(){
+
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+
+//     const showPlayGround = document.getElementById('play-ground');
+//     showPlayGround.classList.remove('hidden');
+// }
+
+function continueGame() {
+    //generate a random alphabet
+    const alphabet = generateRandomAlphabet();
+    console.log(alphabet);
+}
+
+function play() {
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
